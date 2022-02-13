1301d32985b37e90c58882f03aed547149c39413
john petfield <johnmeatjohnmeat@gmail.com>
2022-02-13 15:23:14 +0000

    Add project files.

---
+++ PiholeJP/BashCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiholeJP
+{
+    static public class BashCommand
+    {
+        static public string run(string _action)
+        {
+            //https://stackoverflow.com/questions/46419222/execute-linux-command-on-centos-using-dotnet-core
+
+            string action;
+            if (_action == "enable")
+                action = "enable";
+            else if (_action == "disable")
+                action = "disable";
+            else action = "status";
+            
+            string command = "sudo pihole " + action;
+
+            Console.WriteLine($"command: {command}");
+            
+
+            string result = "";
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+            {
+                proc.StartInfo.FileName = "/bin/bash";
+                proc.StartInfo.Arguments = "-c \" " + command + " \"";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+
+                result += proc.StandardOutput.ReadToEnd();
+                result += proc.StandardError.ReadToEnd();
+
+                proc.WaitForExit();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PiholeJP/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiholeJP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [BindProperty(SupportsGet = true)]
+        public string action { get; set; }
+
+        [BindProperty]
+        public string piholeResponse{ get; set; }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(action))
+            {
+                //action = "status";
+            }
+            else
+            {
+                Console.WriteLine($"action: {action}");
+                string piholeResponse = BashCommand.run(action);
+                Console.WriteLine($"piholeResponse: {piholeResponse}");
+            }
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
